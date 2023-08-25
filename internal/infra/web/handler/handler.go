cbe552126739cb247590ae6bf84547f8a5398ae7
Sup3r-Us3r <maydersonmello@gmail.com>
2023-08-25 19:34:58 -0300

    add new controllers in container

--- internal/infra/web/handler/handler.go
+++ internal/infra/web/handler/handler.go
@@ -11,7 +11,11 @@ import (
 
 func NewHandlerContainer(useCaseContainer usecase.UseCaseContainer, webserver webserver.WebServer) {
 	signInHandler := auth.NewSignInHandler(useCaseContainer.SignInUseCase)
+	getBarberById := barber.NewGetBarberByIdHandler(useCaseContainer.GetBarberByIdUseCase)
+	fetchBarbersHandler := barber.NewFetchBarbersHandler(useCaseContainer.FetchBarbersUseCase)
 	createBarberHandler := barber.NewCreateBarberHandler(useCaseContainer.CreateBarberUseCase)
+	updateBarberHandler := barber.NewUpdateBarberHandler(useCaseContainer.UpdateBarberUseCase)
+	deleteBarberHandler := barber.NewDeleteBarberHandler(useCaseContainer.DeleteBarberUseCase)
 
 	webserver.AddHandler("/docs/*", httpSwagger.Handler(
 		httpSwagger.URL("http://localhost:8080/docs/doc.json")),
@@ -19,5 +23,9 @@ func NewHandlerContainer(useCaseContainer usecase.UseCaseContainer, webserver we
 
 	webserver.AddHandler("/v1/auth/sign-in", signInHandler.Handle)
 
-	webserver.AddHandler("/v1/barber/create", createBarberHandler.Handle, middleware.VerifyToken)
+	webserver.AddHandler("/v1/barber/fetch", fetchBarbersHandler.Handle, middleware.VerifyToken)
+	webserver.AddHandler("/v1/barber/get/{id}", getBarberById.Handle)
+	webserver.AddHandler("/v1/barber/create", createBarberHandler.Handle)
+	webserver.AddHandler("/v1/barber/update/{id}", updateBarberHandler.Handle)
+	webserver.AddHandler("/v1/barber/delete/{id}", deleteBarberHandler.Handle)
 }
