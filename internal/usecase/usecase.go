ad3292b3e052b4e2db8ccb026eb58d23c3ccd3a8
Sup3r-Us3r <maydersonmello@gmail.com>
2023-08-25 19:09:31 -0300

    add new use cases in container

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -7,13 +7,23 @@ import (
 )
 
 type UseCaseContainer struct {
-	SignInUseCase       auth.SignInUseCaseInterface
-	CreateBarberUseCase barber.CreateBarberUseCaseInterface
+	SignInUseCase           auth.SignInUseCaseInterface
+	GetBarberByIdUseCase    barber.GetBarberByIdUseCaseInterface
+	GetBarberByEmailUseCase barber.GetBarberByEmailUseCaseInterface
+	FetchBarbersUseCase     barber.FetchBarbersUseCaseInterface
+	CreateBarberUseCase     barber.CreateBarberUseCaseInterface
+	UpdateBarberUseCase     barber.UpdateBarberUseCaseInterface
+	DeleteBarberUseCase     barber.DeleteBarberUseCaseInterface
 }
 
 func GetUseCases(repositoryContainer repository.RepositoryContainer) *UseCaseContainer {
 	return &UseCaseContainer{
-		SignInUseCase:       auth.NewSignInUseCase(repositoryContainer),
-		CreateBarberUseCase: barber.NewCreateBarberUseCase(repositoryContainer),
+		SignInUseCase:           auth.NewSignInUseCase(repositoryContainer),
+		GetBarberByIdUseCase:    barber.NewGetBarberByIdUseCase(repositoryContainer),
+		GetBarberByEmailUseCase: barber.NewGetBarberByEmailUseCase(repositoryContainer),
+		FetchBarbersUseCase:     barber.NewFetchBarbersUseCase(repositoryContainer),
+		CreateBarberUseCase:     barber.NewCreateBarberUseCase(repositoryContainer),
+		UpdateBarberUseCase:     barber.NewUpdateBarberUseCase(repositoryContainer),
+		DeleteBarberUseCase:     barber.NewDeleteBarberUseCase(repositoryContainer),
 	}
 }
